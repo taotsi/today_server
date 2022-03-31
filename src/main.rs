a45fab90c30cc119bf997d36aec44deba84e1d90
taotsi <taotsi@protonmail.com>
2022-04-01 00:51:26 +0800

    add api path

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    // Match any request and return hello world!
-    let cors = warp::cors()
-      .allow_origin("https://hyper.rs")
-      .allow_methods(vec!["GET", "POST", "DELETE"]);
+    // let cors = warp::cors()
+    //   .allow_origin("https://hyper.rs")
+    //   .allow_methods(vec!["GET", "POST", "DELETE"]);
 
     // let route = warp::any()
     //   .map(warp::reply)
     //   .with(cors);
-    let routes = warp::any()
-      .map(|| "Hello, World!");
+    let hello = warp::path!("api" / "hello")
+      .map(|| format!("hi there!"));
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(hello)
+      .run(([127, 0, 0, 1], 3030))
+      .await;
 }
